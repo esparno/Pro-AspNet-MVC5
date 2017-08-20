5f0bcfcd120eb4f708a0e4ec271b494617bcebfa
esparno <elsparno@gmail.com>
2017-08-19 20:18:31 -0600

    chapter 11 edit method and unit tests

--- SportsStore/SportsStore.WebUI/Controllers/AdminController.cs
+++ SportsStore/SportsStore.WebUI/Controllers/AdminController.cs
@@ -1,5 +1,7 @@
 ﻿using SportsStore.Domain.Abstract;
 using System.Web.Mvc;
+using SportsStore.Domain.Entities;
+using System.Linq;
 
 namespace SportsStore.WebUI.Controllers
 {
@@ -14,6 +16,13 @@ namespace SportsStore.WebUI.Controllers
         public ViewResult Index()
         {
             return View(repository.Products);
+        }
+
+        public ViewResult Edit(int productId)
+        {
+            Product product = repository.Products.FirstOrDefault(p => p.ProductID == productId);
+
+            return View(product);
         }
     }
 }
