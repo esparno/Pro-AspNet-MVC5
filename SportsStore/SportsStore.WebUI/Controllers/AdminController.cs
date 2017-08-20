f0b5b3607cbcac97025c5577d31f2ad878babd42
esparno <elsparno@gmail.com>
2017-08-19 18:52:11 -0600

    chapter 11 create admin controller

---
+++ SportsStore/SportsStore.WebUI/Controllers/AdminController.cs
@@ -0,0 +1,19 @@
+﻿using SportsStore.Domain.Abstract;
+using System.Web.Mvc;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class AdminController : Controller
+    {
+        private IProductRepository repository;
+        public AdminController(IProductRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public ViewResult Index()
+        {
+            return View(repository.Products);
+        }
+    }
+}
