421a1c057377757c63506932e869cf43a58036a3
esparno <elsparno@gmail.com>
2017-08-19 20:54:30 -0600

    chapter 11 add save ability to edit action

--- SportsStore/SportsStore.WebUI/Controllers/AdminController.cs
+++ SportsStore/SportsStore.WebUI/Controllers/AdminController.cs
@@ -18,10 +18,18 @@ namespace SportsStore.WebUI.Controllers
             return View(repository.Products);
         }
 
-        public ViewResult Edit(int productId)
+        [HttpPost]
+        public ActionResult Edit(int productId)
         {
             Product product = repository.Products.FirstOrDefault(p => p.ProductID == productId);
 
+            if(ModelState.IsValid)
+            {
+                repository.SaveProduct(product);
+                TempData["message"] = string.Format("{0} has been saved", product.Name);
+                return RedirectToAction("Index");
+            }
+            
             return View(product);
         }
     }
