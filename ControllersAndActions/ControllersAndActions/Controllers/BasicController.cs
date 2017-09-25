51a2be931f76fce5e88f0e9fedd20b94c7b8da67
esparno <elsparno@gmail.com>
2017-09-25 12:53:47 -0600

    chapter 17 inheriting from IController

---
+++ ControllersAndActions/ControllersAndActions/Controllers/BasicController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+using System;
+
+namespace ControllersAndActions.Controllers
+{
+    public class BasicController : IController
+    {
+
+        public void Execute(RequestContext requestContext)
+        {
+            string controller = (string)requestContext.RouteData.Values["controller"];
+            var val = requestContext.RouteData.Values;
+
+            string action = (string)requestContext.RouteData.Values["action"];
+
+            requestContext.HttpContext.Response.Write(String.Format("Controller: {0}, Action: {1}", controller, action));
+        }
+    }
+}
